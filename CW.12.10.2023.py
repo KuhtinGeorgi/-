73c6c7a569505544141ccfa4f9abb1e03ef32488
KuhtinGeorgi <146083378+KuhtinGeorgi@users.noreply.github.com>
2023-10-12 16:49:14 +0500

    Update CW.12.10.2023.py

--- CW.12.10.2023.py
+++ CW.12.10.2023.py
@@ -1,1 +1,15 @@
-#1
+#1 Ваша задача — создать функцию, которая может принимать в качестве аргумента любое неотрицательное целое число и возвращать его с цифрами в порядке убывания.
+def descending_order(num):
+    counter = 0
+    arr = list(str(num))
+    for i in arr:
+        arr[counter] = int(i)
+        counter += 1
+    arr.sort()
+    arr = list(reversed(arr))
+    ans = ''
+    for i in arr:
+        ans += str(i)
+    return int(ans)
+
+#2 
