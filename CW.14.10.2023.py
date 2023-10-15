6eb199fe10361cfc6dff0ad00d83838d8207e029
KuhtinGeorgi <146083378+KuhtinGeorgi@users.noreply.github.com>
2023-10-15 22:33:01 +0500

    Update CW.14.10.2023.py

--- CW.14.10.2023.py
+++ CW.14.10.2023.py
@@ -18,3 +18,54 @@ def grow(arr):
         result *= num
     return result
   
+#4 вернуть наименьшее число в массиве
+def find_smallest_int(arr):
+    arr.sort()
+    return arr[0]
+
+#5 Вычислите сумму чисел в n-й строке этого треугольника (начиная с индекса 1), например: (Ввод --> Вывод)
+def row_sum_odd_numbers(n):
+    return n * n * n
+
+#6
+def greet(name, owner):
+    if name == owner:
+        return 'Hello boss'
+    else:
+        return 'Hello guest'
+
+#7 Напишите функцию bmi, которая вычисляет индекс массы тела (bmi = вес / рост2).
+def bmi(weight, height):
+    BMI = weight / height ** 2
+    if BMI <= 18.5:
+        return "Underweight"
+    elif BMI <= 25.0:
+        return "Normal"
+    elif BMI <= 30.0:
+        return "Overweight"
+    else:
+        return "Obese"
+
+#8 Узнать квартал месяца
+def quarter_of(month):
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    elif month <= 12:
+        return 4
+
+#9 Напишите функцию, которая берет массив слов, объединяет их в предложение и возвращает предложение.
+def smash(words):
+    return ' '.join(words) + '' 
+
+#10 Ъуь
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 == 0 and flower2 % 2 != 0:
+        return True
+    elif flower2 % 2 == 0 and flower1 % 2 != 0:
+        return True
+    else:
+        return False
