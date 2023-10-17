6f9c8007cd29c0a75896f9870699bfa359278f7b
KuhtinGeorgi <146083378+KuhtinGeorgi@users.noreply.github.com>
2023-10-17 20:29:24 +0500

    Update CW.16.10.2023.py

--- CW.16.10.2023.py
+++ CW.16.10.2023.py
@@ -22,3 +22,31 @@ def get_sum(a,b):
         for i in range(a,b+1):
             soma += i
         return soma
+
+#4 Минимальное и максимальное число в массиве
+def min_max(lst):
+    return [min(lst), max(lst)]
+
+#4 Удалить первый и последний элемент строки
+def remove_char(s):
+    return s[1: -1]
+
+#5 Пирамида
+def tower_builder(n_floors):
+    piramida = []
+    for i in range(1, n_floors + 1):
+        piramida.append(' ' * (n_floors - i) + '*' * (2 * i-1))
+    return piramida
+
+#6 Учитывая строку цифр, вы должны заменить любую цифру ниже 5 на "0", а любую цифру 5 и выше - на "1". Верните результирующую строку.
+def fake_bin(x):
+    result = ""
+    for num in x:
+        if int(num) < 5:
+            result = result + "0"
+        else:
+            result = result + "1"
+    return result
+
+#7
+
